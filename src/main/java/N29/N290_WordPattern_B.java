2b4ca6d4f82d8f33831ed29aad1374f637087468
shuai.li <286287737@qq.com>
2015-10-06 14:57:27 +0800

    long long long

---
+++ src/main/java/N29/N290_WordPattern_B.java
@@ -0,0 +1,26 @@
+package N29;
+
+/**
+ * Created: shuai.li(286287737@qq.com)
+ * Date: 2015-10-06
+ */
+
+/**
+ * Given a pattern and a string str, find if str follows the same pattern.
+ * <p/>
+ * Examples:
+ * pattern = "abba", str = "dog cat cat dog" should return true.
+ * pattern = "abba", str = "dog cat cat fish" should return false.
+ * pattern = "aaaa", str = "dog cat cat dog" should return false.
+ * pattern = "abba", str = "dog dog dog dog" should return false.
+ * Notes:
+ * Both pattern and str contains only lowercase alphabetical letters.
+ * Both pattern and str do not have leading or trailing spaces.
+ * Each word in str is separated by a single space.
+ * Each letter in pattern must map to a word with length that is at least 1.
+ */
+public class N290_WordPattern_B {
+    public boolean wordPattern(String pattern, String str) {
+        return false;
+    }
+}
